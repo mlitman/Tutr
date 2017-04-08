f94d5ce652515a336cc0dce08af1bf3d584d9c1e
Mike Litman <awesomefat@gmail.com>
2017-04-08 09:27:28 -0500

    Initial commit

---
+++ app/src/main/java/awesomefat/com/tutr/MainActivity.java
@@ -0,0 +1,40 @@
+package awesomefat.com.tutr;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void askButton(View v)
+    {
+        Intent i = new Intent(this, askAQuestion.class);
+        this.startActivity(i);
+    }
+
+    public void answerButton(View v)
+    {
+        Intent i = new Intent(this, answerAQuestion.class);
+        this.startActivity(i);
+    }
+    public void viewProfileButton (View v)
+    {
+        Intent i = new Intent (this, viewProfile.class);
+        this.startActivity(i);
+    }
+}
+
+
+
+
+
