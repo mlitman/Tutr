8f08dba972fb47976e07167a9afafc058aae0817
alexisrolenc <alexis.rolenc@gmail.com>
2017-05-04 10:32:57 -0500

    alexi changed

--- app/src/main/java/awesomefat/com/tutr/Core.java
+++ app/src/main/java/awesomefat/com/tutr/Core.java
@@ -1,6 +1,9 @@
 package awesomefat.com.tutr;
 
 import java.util.ArrayList;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by mumbl1 on 4/8/17.
@@ -13,4 +16,5 @@ public class Core
     static ArrayList<Question> theQuestions = new ArrayList<Question>();
     static ArrayList<Answer> theAnswers = new ArrayList<Answer>();
     static ArrayList<MyQuestion> theMyQuestions = new ArrayList<MyQuestion>();
+    static FirebaseUser currentUser;
 }
--- app/src/main/java/awesomefat/com/tutr/Register.java
+++ app/src/main/java/awesomefat/com/tutr/Register.java
@@ -1,13 +1,77 @@
 package awesomefat.com.tutr;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Register extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static awesomefat.com.tutr.R.layout.activity_main;
+import static awesomefat.com.tutr.R.layout.activity_register;
+
+public class Register extends AppCompatActivity
+{
+
+    private FirebaseAuth mAuth;
+    private EditText signUpemail;
+    private EditText signUpPassword;
+    private Button signUpButton;
+    private Register registerActivity;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        this.signUpButton = (Button)this.findViewById(R.id.signUpButton);
+        this.signUpemail = (EditText)this.findViewById(R.id.emailET);
+        this.signUpPassword = (EditText)this.findViewById(R.id.passwordET);
+        this.registerActivity = this;
+    }
+
+    public void signUpButtonTapped(View v)
+    {
+       if(signUpemail.getText().toString() == "" || signUpPassword.getText().toString() == "")
+       {
+           System.out.println("Enter in email/password");
+       }
+       else
+       {
+           mAuth.createUserWithEmailAndPassword(signUpemail.getText().toString(), signUpPassword.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+           {
+               @Override
+               public void onComplete(@NonNull Task<AuthResult> task)
+               {
+                   if(task.isSuccessful())
+                   {
+                       //Log.d(TAG, "createUserWithEmail:success");
+                       Core.currentUser = mAuth.getCurrentUser();
+                       //updateUI(null);
+                       Intent i = new Intent(registerActivity, MainActivity.class);
+                       registerActivity.startActivity(i);
+                   }
+                   else
+                   {
+                       //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                       Toast.makeText(registerActivity, "Sign Up Failed Make Sure All Fields are Filled.", Toast.LENGTH_SHORT).show();
+                      // updateUI(null);
+
+                   }
+               }
+           });
+       }
+
+
     }
 }
