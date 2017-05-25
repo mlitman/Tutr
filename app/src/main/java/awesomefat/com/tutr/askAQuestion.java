ac36015b72e3f992e33068c5b763c8e7c6cc0f21
alexisrolenc <alexis.rolenc@gmail.com>
2017-05-25 18:49:58 -0500

    alexis changed

--- app/src/main/java/awesomefat/com/tutr/askAQuestion.java
+++ app/src/main/java/awesomefat/com/tutr/askAQuestion.java
@@ -14,10 +14,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class askAQuestion extends AppCompatActivity
 {
-    private ListView courseList;
-    private String[] courses = {"CSC150","CSC175","CSC200","CSC250","CSC300","CSC150","CSC175","CSC200","CSC250","CSC300","CSC150","CSC175","CSC200","CSC250","CSC300"};
     private QuestionList qList;
-    private TextView currCourseTV;
+    private TextView currCourseET;
     private EditText titleAskET;
     private EditText bodyAskET;
 
@@ -26,26 +24,15 @@ public class askAQuestion extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ask_aquestion);
-        this.currCourseTV = (TextView)this.findViewById(R.id.currCourseTV);
+        this.currCourseET = (EditText)this.findViewById(R.id.currCourseET);
         this.titleAskET = (EditText)this.findViewById(R.id.titleAskET);
         this.bodyAskET = (EditText)this.findViewById(R.id.bodyAskET);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,this.courses);
-        this.courseList = (ListView)this.findViewById(R.id.courseList);
-        this.courseList.setAdapter(adapter);
-        this.courseList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                currCourseTV.setText(courses[position]);
-            }
-        });
-
-
     }
 
     public void submitQuestionButtonPressed(View v)
     {
-        Question questionToSave = new Question(titleAskET.getText().toString(), bodyAskET.getText().toString(), currCourseTV.getText().toString());
-        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("questions").child(currCourseTV.getText().toString()).push();
+        Question questionToSave = new Question(titleAskET.getText().toString(), bodyAskET.getText().toString(), currCourseET.getText().toString());
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("questions").child(currCourseET.getText().toString()).push();
         mDatabase.setValue(questionToSave);
         Core.theQuestions.add(questionToSave);
         Intent i = new Intent(this, QuestionList.class);
