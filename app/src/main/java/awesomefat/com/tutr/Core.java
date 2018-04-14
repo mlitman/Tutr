14b477af051ed227bfefb992a0783c8e81483d5e
alexisrolenc <alexis.rolenc@gmail.com>
2018-04-14 08:36:33 -0500

    alexis changed

--- app/src/main/java/awesomefat/com/tutr/Core.java
+++ app/src/main/java/awesomefat/com/tutr/Core.java
@@ -5,10 +5,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-/**
- * Created by mumbl1 on 4/8/17.
- */
-
 public class Core
 {
     static Question selectedQuestion = null;
